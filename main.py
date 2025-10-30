f9522cb2f70612a44657c05a9ce847dde47a4831
Viktor <vitor_a.flora@outlook.com>
2025-10-30 01:03:07 -0300

    Bug fix search by id

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import pandas as pd
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from typing import Optional
-from fastapi.exceptions import HTTPException
 
+# Cria uma instância da aplicação FastAPI
 app = FastAPI(
     title="API de Livros",
-    description="API para consulta de dados de livros",
+    description="Uma API para consulta de dados de livros extraídos do site 'books.tscrape.com'",
     version="1.0.0"
 )
 
 CSV_FILE_PATH = 'data/books.csv'
 
 try:
-    df_books = pd.read_csv(CSV_FILE_PATH).reset_index().rename(columns={'index': 'id'})
+    df_books = pd.read_csv(CSV_FILE_PATH)
+    df_books['id'] = df_books['id'].astype(int)         # erro de leitura como string
+    df_books.set_index('id', inplace=True) 
+    
 except FileNotFoundError:
     print(f"Erro: O arquivo '{CSV_FILE_PATH}' não foi encontrado. Execute o scraper primeiro.")
+    df_books = pd.DataFrame()
+except Exception as e:
+    print(f"Erro ao carregar e processar o DataFrame: {e}")
     df_books = pd.DataFrame()
 
 
@@ -23,7 +29,7 @@ except FileNotFoundError:
 def read_root():
     """Endpoint raiz que exibe uma mensagem de boas-vindas."""
     return {"messages": [
-        "Bem-vindo à API de Livros! Acesse /docs para ver a documentação interativa.",
+        "Bem-vindo à API de Livros! Acesse /docs para ver a documentação interativa (Swagger).",
         "Acesse /api/v1/books para ver a lista de livros.",
         "Acesse /api/v1/categories para ver a lista de categorias.",
         "Use /api/v1/books/search?title=nome&category=categoria para buscar livros por título e/ou categoria."
@@ -39,7 +45,9 @@ def get_all_books():
     """Retorna uma lista de todos os livros disponíveis na base de dados."""
     if df_books.empty:
         return {"message": "Nenhum dado de livro encontrado."}
-    return df_books.to_dict('records')
+    
+    df_with_id = df_books.reset_index()
+    return df_with_id.to_dict('records')
 
 @app.get("/api/v1/categories")
 def get_categories():
@@ -54,7 +62,6 @@ def get_categories():
 def search_books(title: Optional[str] = None, category: Optional[str] = None):
     """
     Busca livros por título e/ou categoria.
-    Ambos os parâmetros são opcionais.
     """
     results = df_books.copy()
 
@@ -70,13 +77,22 @@ def search_books(title: Optional[str] = None, category: Optional[str] = None):
     if results.empty:
         return {"message": "Nenhum livro encontrado com os critérios fornecidos."}
 
-    return results.to_dict('records')
+    results_with_id = results.reset_index()
+    return results_with_id.to_dict('records')
 
 @app.get("/api/v1/books/{book_id}")
 def get_book_by_id(book_id: int):
     """Retorna os detalhes de um livro específico pelo seu ID."""
-    if book_id not in df_books['id'].values:
-        raise HTTPException(status_code=404, detail="Livro com o ID fornecido não encontrado.")
     
-    book = df_books[df_books['id'] == book_id].to_dict('records')
-    return book[0]
+    if df_books.empty:
+         raise HTTPException(status_code=503, detail="Dados de livros não carregados.")
+
+    try:
+        book_series = df_books.loc[book_id]
+        book_data = book_series.to_dict()
+        book_data['id'] = book_id
+        
+        return book_data
+        
+    except KeyError:
+        raise HTTPException(status_code=404, detail="Livro com o ID fornecido não encontrado.")
